9cd71202c625e1b0594416e5a8832e6ea1580f3b
kurniawan <codechrl@gmail.com>
2023-03-15 23:00:13 +0700

    util-update-schema-response

--- schema/message.py
+++ schema/message.py
@@ -1,25 +1,67 @@
-from fastapi import HTTPException
+from typing import List, TypeVar, Union
+
 from pydantic import BaseModel
+
+from schema.category import Category
+from schema.category_product import CategoryProductDetail
+from schema.product import Product
+
+T = TypeVar("T")
+
+
+class ValidationError(BaseModel):
+    loc: list = None
+    msg: str = None
+    type: str = None
 
 
 class BasicMessage(BaseModel):
     message: str
 
 
+class IdMessage(BaseModel):
+    id: int
+
+
+class CategoryProductIdMessage(BaseModel):
+    product_id: int
+    category_id: int
+
+
 class Pagination(BaseModel):
     page: int
     limit: int
-    total: int
+    total_page: int
+    total_all: int
 
 
 class Response(BaseModel):
-    status: str
-    code: int
-    message: str = None
-    data: list[dict] = None
+    status: str = "success"
+    code: int = 200
+    message: Union[dict, str, List[ValidationError]] = None
+    data: Union[List[T], IdMessage, CategoryProductIdMessage] = None
     pagination: Pagination = None
 
 
-class ResponseHTTPException(HTTPException):
-    def __init__(self, message):
-        super().__init__(status="error", code=500, message=message)
+class ValidationErrorResponse(Response):
+    message: List[ValidationError] = None
+
+
+class IdResponse(Response):
+    data: IdMessage = None
+
+
+class CategoryProductIdResponse(Response):
+    data: CategoryProductIdMessage = None
+
+
+class CategoriesResponse(Response):
+    data: List[Category] = None
+
+
+class ProductResponse(Response):
+    data: List[Product] = None
+
+
+class CategoryProductDetailResponse(Response):
+    data: List[CategoryProductDetail] = None
